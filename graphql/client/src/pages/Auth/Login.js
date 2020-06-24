7801c2c02b97052e55e5ce7abc3c4ecf0d680f5a
mmyadav <mintu08.yadav@gmail.com>
2020-06-24 17:53:22 +0530

    verification email using firebase

--- graphql/client/src/pages/Auth/Login.js
+++ graphql/client/src/pages/Auth/Login.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = () => {
+        //
+    };
+
     return (
-        <div className="container">
-            <div className="p-5">
+        <div className="contianer">
+            <div className="row p-5">
                 <h4>Login</h4>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>Email Address</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="form-control"
+                            placeholder="Enter email"
+                            disabled={loading}
+                        />
+                    </div>
+                </form>
             </div>
         </div>
-    )
-}
+    );
+};
+
 export default Login;
--- graphql/client/src/pages/Auth/Register.js
+++ graphql/client/src/pages/Auth/Register.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../../firebase';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        const config = {
+            url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+            handleCodeInApp: true
+        }
+        await auth.sendSignInLinkToEmail(email, config)
+
+        // notification for verification
+
+        // set email in local storage
+        window.localStorage.setItem('emailForRegisteration', email);
+
+        // clear state
+        setEmail('');
+        setLoading('');
+    };
+
     return (
-        <div className="container">
-            <div className="p-5">
-                <h4>Register</h4>
-            </div>
+        <div className="contianer p-5">
+            <h4>Register</h4>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Email Address</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="form-control"
+                        placeholder="Enter email"
+                        disabled={loading}
+                    />
+                </div>
+                <button className="btn btn-raised btn-primary" disabled={!email || loading}>
+                    Submit
+                </button>
+            </form>
         </div>
-    )
-}
+    );
+};
+
 export default Register;
